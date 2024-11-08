f1404079aecdee2c817da850111052f09591c6fa
Piero Toffanin <pt@masseranolabs.com>
2024-11-08 15:21:56 -0500

    Grouping

--- openfinprep/app.py
+++ openfinprep/app.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, Flask, render_template, make_response
+from flask import Blueprint, Flask, render_template, make_response, jsonify, current_app
 from edgar.reference.tickers import get_company_ticker_name_exchange 
 from edgar import set_identity, use_local_storage
 import yaml
 
 def create_app(args):
-    bp = Blueprint('Main app', __name__)
+    bp = Blueprint('ofp', __name__)
     set_identity("openfinprep@openfinprep.org")
     if args.use_local_storage:
         use_local_storage()
@@ -42,18 +42,30 @@ def create_app(args):
 
     @bp.route("/endpoints")
     def endpoints():
+        """
+         - Stock List
+         - Another group
+        """
         ep = [
             stock_list,
             cik_list,
         ]
 
-        for e in ep:
-            specs = yaml.safe_load(e.__doc__)
-        print(specs)
-
-        # TODO: group/endpoint sorting order?
         
-        return "{}"
+        groups = yaml.safe_load(endpoints.__doc__)
+        output = [{'group': g, 'endpoints': []} for g in groups]
+
+        urls = current_app.url_map.bind("openfinprep.org", "/")
+        specs = []
+
+        for e in ep:
+            spec = yaml.safe_load(e.__doc__)
+            group_id = groups.index(spec['group'])
+            del spec['group']
+            spec['url'] = urls.build("ofp." + e.__name__, {})
+            output[group_id]['endpoints'].append(spec)
+
+        return jsonify(output)
 
     app = Flask(__name__)
 
